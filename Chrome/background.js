08cb35d49a4d6917aa2161a50928a7766073d89e
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2023-02-08 21:54:11 +0530

    hg

--- Chrome/background.js
+++ Chrome/background.js
@@ -1,2 +1,6 @@
+importScripts("lib/config.js");
+importScripts("lib/chrome.js");
+importScripts("lib/runtime.js");
+importScripts("lib/common.js");
 importScripts("interface/lib/browserDetector.js");
 importScripts("cookie-editor.js");
